42e02dede50c6dfaf76a215e08bca1f304c13dec
sebadorn <seba_dorn@gmx.de>
2018-08-18 02:06:24 +0200

    - Better fog: Gets gradually darker. - Performance: Generate ground and fog as single images beforehand instead of using single tiles.

--- dev/i.js
+++ dev/i.js
@@ -48,12 +48,7 @@ class Char {
 		) {
 			this.x -= x;
 			this.y -= y;
-
-			return;
 		}
-
-		this.s.x += x * g.tw;
-		this.s.y += y * g.tw;
 	}
 
 
@@ -66,15 +61,13 @@ class Tile {
 	/**
 	 *
 	 * @constructor
-	 * @param {number} t  - Underground type.
-	 * @param {number} xi - X index on map.
-	 * @param {number} yi - Y index on map.
-	 * @param {number} x  - X coord [px].
-	 * @param {number} y  - Y coord [px].
+	 * @param {number} t - Underground type.
+	 * @param {number} x - X coord [px] and X index on the map.
+	 * @param {number} y - Y coord [px] and Y index on the map.
 	 */
-	constructor( t, xi, yi, x, y ) {
-		this.x = xi;
-		this.y = yi;
+	constructor( t, x, y ) {
+		this.x = x;
+		this.y = y;
 
 		// Type:
 		// 2 - Grass, walkable.
@@ -113,25 +106,29 @@ class Tile {
 	 * Update the tile.
 	 * @param {Char} player
 	 */
-	update( player ) {
+	toShadow( player ) {
+		// Update fog.
+
+		// Distance of tile to the player.
 		let dx = Math.abs( player.x - this.x );
 		let dy = Math.abs( player.y - this.y );
+		// Euclidean distance.
+		let de = Math.sqrt( dx * dx + dy * dy );
 
-		// Update fog.
-		this.s.color = this.c;
+		// Start the fog (darken a tile) starting
+		// at 3 fields from the player.
+		let f = Math.min( 3 / de, 1 );
 
-		if( dx > 2 || dy > 2 || dx + dy == 4 ) {
-			let f = 0.1;
+		// c = c.map( v => ~~( v * f ) );
 
-			if( dx > 5 || dy > 5 || dx + dy > 8 ) {
-				f = 0.05;
-			}
+		this.s.color = `rgba(0,0,0,0.5)`;
 
-			let c = this.c.substr( 4, this.c.length - 5 ).split( ',' );
-			c = c.map( v => ~~( v * f ) );
 
-			this.s.color = `rgb(${c.join( ',' )})`;
-		}
+		// Update position. Move so the
+		// player is always in the center.
+		this.s.x = this.x - player.x;
+		this.s.y = this.y - player.y;
+
 
 		this.s.update();
 	}
@@ -146,12 +143,14 @@ class Tile {
 	window.g = {
 		rnd: Math.random,
 		k: kontra,
-		mc: 48, // number of map columns
-		mr: 30, // number of map rows
+		mc: 20, // number of map columns
+		mr: 20, // number of map rows
 		tw: 32, // tile width (and height) [px]
 		ww: window.innerWidth, // window width
 		wh: window.innerHeight // window height
 	};
+	g.mw = g.mc * g.tw; // map width [px]
+	g.mh = g.mr * g.tw; // map height [px]
 
 
 	// Generate map.
@@ -165,36 +164,72 @@ class Tile {
 	}
 
 
-	// Setup
-	g.k.init();
-	g.k.canvas.width = g.ww;
-	g.k.canvas.height = g.wh;
+	// Generate static ground. For performance
+	// reasons render all the tiles only once
+	// and create a new image, which is then used
+	// and just moved around.
+
+	g.k.init( document.getElementById( 'ground' ) );
+	g.k.canvas.width = g.mc;
+	g.k.canvas.height = g.mr;
+	g.k.context.imageSmoothingEnabled = false;
 
 	let tiles = [];
 	let yi = 0;
-	let x = ~~( ( g.ww - g.mc * g.tw ) / 2 );
-	let y = ~~( ( g.wh - g.mr * g.tw ) / 2 );
+
+	g.xoff = ~~( ( g.ww - g.tw ) / 2 );
+	g.yoff = ~~( ( g.wh - g.tw ) / 2 );
 
 	map.forEach( ( v, i ) => {
 		let xi = i % g.mc;
-
-		tiles.push(
-			new Tile(
-				v,
-				xi, yi,
-				x + g.tw * xi,
-				y + g.tw * yi
-			)
-		);
-
+		tiles.push( new Tile( v, xi, yi ) );
 		yi += !( ++i % g.mc );
 	} );
 
-	let player = new Char(
-		10, 10,
-		x + g.tw * 10,
-		y + g.tw * 10
-	);
+	tiles.forEach( t => { t.s.render(); } );
+
+	let groundCanvas = g.k.canvas;
+
+
+	// Now create a shadow overlay.
+
+	let shadowCanvas = document.getElementById( 'shadow' );
+	shadowCanvas.width = g.mc;
+	shadowCanvas.height = g.mr;
+
+	let shadowCtx = shadowCanvas.getContext( '2d' );
+	shadowCtx.imageSmoothingEnabled = false;
+
+	for( let y = 0; y < g.mr; y++ ) {
+		for( let x = 0; x < g.mc; x++ ) {
+			// Distance of tile to the player.
+			let dx = Math.abs( 10 - x );
+			let dy = Math.abs( 10 - y );
+			// Euclidean distance.
+			let de = Math.sqrt( dx * dx + dy * dy );
+
+			// Start the fog (darken a tile) starting
+			// at 3 fields from the player.
+			let f = 1 - Math.min( 3 / de, 1 );
+
+			shadowCtx.fillStyle = `rgba(0,0,0,${f})`;
+			shadowCtx.fillRect( x, y, 1, 1 );
+		}
+	}
+
+
+	// Initialize again, this time with the main canvas.
+
+	g.k.init();
+	g.k.canvas.width = g.ww;
+	g.k.canvas.height = g.wh;
+	g.k.context.imageSmoothingEnabled = false;
+
+	let ground = g.k.sprite( { image: groundCanvas } );
+	let shadow = g.k.sprite( { image: shadowCanvas } );
+
+	let player = new Char(0, 0, g.xoff, g.yoff);
+	player.mv( 10, 10 );
 
 	g.k.keys.bind( 'left', () => player.mv( -1, 0 ) );
 	g.k.keys.bind( 'right', () => player.mv( 1, 0 ) );
@@ -204,12 +239,27 @@ class Tile {
 	let loop = g.k.gameLoop( {
 
 		update: () => {
-			tiles.forEach( t => t.update( player ) );
 			player.s.update();
 		},
 
 		render: () => {
-			tiles.forEach( t => t.s.render() );
+			let overlayX = g.xoff - player.x * g.tw;
+			let overlayY = g.yoff - player.y * g.tw;
+
+			// Draw the ground image but upscale it.
+			// The tiles in the original are only 1x1px.
+			g.k.context.drawImage(
+				ground.image,
+				overlayX, overlayY,
+				g.mw, g.mh
+			);
+
+			g.k.context.drawImage(
+				shadow.image,
+				g.xoff, g.yoff,
+				g.mw, g.mh
+			);
+
 			player.s.render();
 		}
 
